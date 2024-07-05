1530acc2cb65ae1975c539c8b9dc84ff7c8f84e8
pRaji9 <pamalaraji9@gmail.com>
2024-07-05 11:35:32 +0530

    code.navbar

---
+++ Navbar.js
@@ -0,0 +1,13 @@
+const Navbar  = () => {
+    return ( 
+        <div className ="navbar">
+            
+                <a href ="#">Contact Us </a>
+                <a href ="#">About Us| </a>
+                <a href ="#">Home| </a>
+            </div>
+        
+     );
+}
+ 
+export default Navbar;
